afda3d24e49737c3dd1156c238d02cc74cc3a03d
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2025-05-11 19:50:13 -0700

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftRESTKit",
+    products: [
+        .library(
+            name: "SwiftRESTKit",
+            targets: ["SwiftRESTKit"]),
+    ],
+    targets: [
+        .target(
+            name: "SwiftRESTKit"),
+    ]
+)
